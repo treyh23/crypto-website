d7a6fc457d6940f07f75538dbd3c5d053e21de53
treyh23 <forrest@contempows.com>
2018-04-16 16:13:01 -0700

    added

--- crypto-site/app/controllers/home_controller.rb
+++ crypto-site/app/controllers/home_controller.rb
@@ -1,7 +1,26 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP","ETH","XLM",]
   end
 
   def about
+  end
+
+  def lookup
+    @symbol = params[:sym]
+    
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ""
+      @symbol = "Hey you forgot to enter a currency!"
+    end
   end
 end
--- crypto-site/config/routes.rb
+++ crypto-site/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 root 'home#index'
+get 'home/lookup'
 get 'home/about'
+post '/home/lookup' => 'home/lookup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

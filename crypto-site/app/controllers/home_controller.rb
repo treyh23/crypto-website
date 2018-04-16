77fe3fbb7d778d51978b6751376c536e9bd49572
treyh23 <forrest@contempows.com>
2018-04-16 16:36:05 -0700

    adding devise

--- crypto-site/app/controllers/home_controller.rb
+++ crypto-site/app/controllers/home_controller.rb
@@ -13,6 +13,13 @@ class HomeController < ApplicationController
   end
 
   def lookup
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin= JSON.parse(@response)
+
     @symbol = params[:sym]
     
     if @symbol

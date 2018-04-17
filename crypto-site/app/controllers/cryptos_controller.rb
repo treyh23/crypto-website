5aea3d7ae207e1b297cb01a4f1190bf9649a385a
treyh23 <forrest@contempows.com>
2018-04-17 10:41:10 -0700

    updated form

--- crypto-site/app/controllers/cryptos_controller.rb
+++ crypto-site/app/controllers/cryptos_controller.rb
@@ -1,10 +1,19 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
+
 
   # GET /cryptos
   # GET /cryptos.json
   def index
     @cryptos = Crypto.all
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_crypto = JSON.parse(@response)
   end
 
   # GET /cryptos/1
@@ -70,5 +79,10 @@ class CryptosController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def crypto_params
       params.require(:crypto).permit(:symbol, :user_id, :cost_per, :amount_owned)
+    end
+
+    def correct_user
+      @correct = current_user.cryptos.find_by(id: params[:id])
+      redirect_to cryptos_path, notice: "Not Authorized To Edit This data" if @correct.nil?
     end
 end
